ca2378c6e93c55068a733523dd1be801cf62139f
Zack Smith <zack.smith@earnst.com>
2020-05-13 15:26:35 -0600

    add gh-pages deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: '/maxout-block-calculator',
   siteMetadata: {
     title: `Maxout Block Calculator`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
